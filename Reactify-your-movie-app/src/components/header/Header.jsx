53af0b7effc0d2779ae65f8c6778c46390b805fd
EmundLiljestrand <emund.l@hotmail.com>
2025-04-15 12:50:38 +0200

    First set of styling

--- Reactify-your-movie-app/src/components/header/Header.jsx
+++ Reactify-your-movie-app/src/components/header/Header.jsx
@@ -1,61 +1,65 @@
-import './header.css'
-import { Link } from 'react-router-dom';
+import "./header.css";
+import { Link } from "react-router-dom";
 import { useState, useEffect } from "react";
 import axios from "axios";
+import movieIcon from "../../assets/movie-icon.png";
 
 function Header() {
+    const [query, setQuery] = useState("");
+    const [searchInput, setSearchInput] = useState("");
 
-  const [query, setQuery] = useState('');
-  const [searchInput, setSearchInput] = useState('');
+    const apiUrl = "https://www.omdbapi.com/";
+    const apiKey = "1a195302";
 
-  const apiUrl = 'https://www.omdbapi.com/';
-  const apiKey = '1a195302';
+    useEffect(() => {
+        if (!searchInput) return;
 
-  useEffect(() => {
-    if (!searchInput) return;
+        axios
+            .get(apiUrl, {
+                params: {
+                    apikey: apiKey,
+                    s: searchInput,
+                },
+            })
+            .then((response) => {
+                if (response.data.Response === "True") {
+                    console.log("Sökresultat:", response.data.Search);
+                } else {
+                    console.log("Inget resultat:", response.data.Error);
+                }
+            })
+            .catch((error) => console.error("API-fel:", error));
+    }, [searchInput]);
 
-    axios
-      .get(apiUrl, {
-        params: {
-          apikey: apiKey,
-          s: searchInput,
-        },
-      })
-      .then((response) => {
-        if (response.data.Response === 'True') {
-          console.log('Sökresultat:', response.data.Search);
-        } else {
-          console.log('Inget resultat:', response.data.Error);
-        }
-      })
-      .catch((error) => console.error('API-fel:', error));
-  }, [searchInput]);
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    setSearchInput(query);
-  };
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setSearchInput(query);
+    };
 
     return (
         <header className="header">
-          <h1 className="">MovieApp</h1>
-          <form onSubmit={handleSubmit} className="header__form">
-            <input
-              type="text"
-              placeholder="Sök film..."
-              value={query}
-              onChange={(e) => setQuery(e.target.value)}
-              className="header__form-input"
-            />
-            <button type="submit" className="header__form-btn">
-              Sök
-            </button>
-          </form>
-          <nav>
-            <Link className='header__nav' to="/watchlist">Watch List</Link>
-          </nav>
+            <Link to="/">
+                <img src={movieIcon} alt="Movie-app icon" class="movie-icon" />
+            </Link>
+            <form onSubmit={handleSubmit} className="header__form">
+                <input
+                    type="text"
+                    placeholder="Sök film..."
+                    value={query}
+                    onChange={(e) => setQuery(e.target.value)}
+                    className="header__form-input"
+                />
+                <button type="submit" className="header__form-btn">
+                    Sök
+                </button>
+            </form>
+            <nav>
+                <Link className="header__nav" to="/watchlist">
+                    Watch List
+                </Link>
+            </nav>
         </header>
-      );
+    );
 }
 
-export default Header
+export default Header;
