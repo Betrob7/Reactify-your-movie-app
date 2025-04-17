2b02927a7b5b7667ebc02e4d4efceaf98a8ffd48
Annelie Östelid <Annelie.ostelid@outlook.com>
2025-04-17 09:38:30 +0200

    added roledescription to carousel

--- Reactify-your-movie-app/src/components/carousel/Carousel.jsx
+++ Reactify-your-movie-app/src/components/carousel/Carousel.jsx
@@ -4,52 +4,52 @@ import Slider from "react-slick";
 import "./carousel.css";
 
 function Carousel({ homePageMovies }) {
-    const settings = {
-        className: "center",
-        centerMode: true,
-        infinite: true,
-        centerPadding: "60px",
-        slidesToShow: 3,
-        speed: 500,
-        lazyload: true,
-        responsive: [
-            {
-                breakpoint: 768,
-                settings: {
-                    slidesToShow: 2,
-                    centerPadding: "40px",
-                },
-            },
-            {
-                breakpoint: 480,
-                settings: {
-                    slidesToShow: 1,
-                    centerPadding: "20px",
-                },
-            },
-        ],
-    };
+  const settings = {
+    className: "center",
+    centerMode: true,
+    infinite: true,
+    centerPadding: "60px",
+    slidesToShow: 3,
+    speed: 500,
+    lazyload: true,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+          centerPadding: "40px",
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          centerPadding: "20px",
+        },
+      },
+    ],
+  };
 
-    return (
-        <div className="slider-container">
-            <Slider {...settings}>
-                {homePageMovies.slice(0, 5).map((movie, index) => (
-                    <div key={index}>
-                        <iframe
-                            className="carousel-video"
-                            width="100%"
-                            height="315"
-                            src={movie.Trailer_link}
-                            title={movie.Title}
-                            frameBorder="0"
-                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                            allowFullScreen
-                        ></iframe>
-                    </div>
-                ))}
-            </Slider>
-        </div>
-    );
+  return (
+    <div className="slider-container">
+      <Slider {...settings} aria-roledescription="carousel">
+        {homePageMovies.slice(0, 5).map((movie, index) => (
+          <div key={index}>
+            <iframe
+              className="carousel-video"
+              width="100%"
+              height="315"
+              src={movie.Trailer_link}
+              title={movie.Title}
+              frameBorder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowFullScreen
+            ></iframe>
+          </div>
+        ))}
+      </Slider>
+    </div>
+  );
 }
 
 export default Carousel;
